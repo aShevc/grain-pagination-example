af7bbae51e065ec79ad1f0f04e7d97f959f93a98
Andrey Shevchenko <andrey.shevchenko@sysgears.com>
2014-01-31 17:00:59 +0200

    Updated site

---
+++ config.rb
@@ -0,0 +1,20 @@
+
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/"
+http_images_path = "/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/fonts"
+css_path = "/home/andrey/projects/grain-pagination-example/.cache/compass/stylesheets"
+
+# Local development paths
+sass_path = "/home/andrey/projects/grain-pagination-example/theme/sass"
+generated_images_path = "/home/andrey/projects/grain-pagination-example/.cache/compass/images"
+images_path = "/home/andrey/projects/grain-pagination-example/theme/images"
+fonts_path = "/home/andrey/projects/grain-pagination-example/theme/fonts"
+
+line_comments = false
+output_style = :compressed
